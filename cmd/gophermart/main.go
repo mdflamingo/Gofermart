070a8621e8927f3b172e4cb739760716c657b685
Anastasiya <aiskoskina@yandex.ru>
2026-01-31 19:31:38 +0300

    init

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,51 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/mdflamingo/Gofermart/internal/config"
+	"github.com/mdflamingo/Gofermart/internal/logger"
+	"github.com/mdflamingo/Gofermart/internal/repository"
+	"github.com/mdflamingo/Gofermart/internal/handler"
+	"go.uber.org/zap"
+
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	conf := config.ParseFlags()
+	if err := run(conf); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(conf *config.Config) error {
+	if err := logger.Initialize(conf.LogLevel); err != nil {
+		return err
+	}
+
+	logger.Log.Info("Running server", zap.String("address", conf.RunAddr))
+	logger.Log.Info("Base short URL", zap.String("url", conf.BaseShortURL))
+
+	storage, err := initStorage(conf)
+	if err != nil {
+		logger.Log.Fatal("Failed to create storage", zap.Error(err))
+	}
+	defer storage.Close()
+
+	r := handler.NewRouter(conf, storage)
+
+	return http.ListenAndServe(conf.RunAddr, r)
+}
+
+func initStorage(conf *config.Config) (*repository.DBStorage, error) {
+	if conf.DataBaseDSN != "" {
+		logger.Log.Info("Attempting to use database storage", zap.String("dsn", conf.DataBaseDSN))
+		if storage, err := repository.NewDBStorage(conf.DataBaseDSN); err == nil {
+			logger.Log.Info("Successfully initialized database storage")
+			return storage, nil
+		}
+		logger.Log.Warn("Failed to initialize database storage")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/mdflamingo/Gofermart
+
+go 1.25.3
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.19.1
+	go.uber.org/zap v1.27.1
+)
+
+require (
+	github.com/go-chi/chi/v5 v5.2.4 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/sync v0.18.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+)
+
+require (
+	github.com/jackc/pgerrcode v0.0.0-20250907135507-afb5586c32a6
+	github.com/jackc/pgx/v5 v5.8.0
+	github.com/mdflamingo/url-shortener v0.0.0-20260128084709-57288818aefe
+	go.uber.org/multierr v1.10.0 // indirect
+)
